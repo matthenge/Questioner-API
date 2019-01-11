63eb75ad617080fbead24b0798ed8e690f56878d
matthenge <matthenge@gmail.com>
2019-01-11 07:01:21 +0300

    [finishes #163069877]finished fetch single meetup endpoint

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,9 +1,10 @@
 from flask import Blueprint
 from flask_restful import Api, Resource
-from app.api.v1.views.meetup_views import AllMeetups
+from app.api.v1.views.meetup_views import AllMeetups, OneMeetup
 
 version1 = Blueprint('v1', __name__, url_prefix='/api/v1')
 
 api = Api(version1, catch_all_404s=True)
 
 api.add_resource(AllMeetups, '/meetups', strict_slashes=False)
+api.add_resource(OneMeetup, '/meetups/<int:meetupId>', strict_slashes=False)
