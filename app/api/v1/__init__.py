75e067c87c8839353867327122af1058d2ad6910
matthenge <matthenge@gmail.com>
2019-01-11 10:20:58 +0300

    [feature #163069624]Added Post question Endpoint

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,6 +1,7 @@
 from flask import Blueprint
 from flask_restful import Api, Resource
 from app.api.v1.views.meetup_views import AllMeetups, OneMeetup
+from app.api.v1.views.question_views import AllQuestions
 
 version1 = Blueprint('v1', __name__, url_prefix='/api/v1')
 
@@ -8,3 +9,4 @@ api = Api(version1, catch_all_404s=True)
 
 api.add_resource(AllMeetups, '/meetups', strict_slashes=False)
 api.add_resource(OneMeetup, '/meetups/<int:meetupId>', strict_slashes=False)
+api.add_resource(AllQuestions, '/questions', strict_slashes=False)
