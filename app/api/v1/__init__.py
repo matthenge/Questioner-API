ab7940f4004a893fdea73b7f444eae97fae6e156
matthenge <matthenge@gmail.com>
2019-01-12 08:49:18 +0300

    [finishes #163070381]Add downvote Endpoint

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,7 +1,8 @@
 from flask import Blueprint
 from flask_restful import Api, Resource
 from app.api.v1.views.meetup_views import AllMeetups, OneMeetup
-from app.api.v1.views.question_views import AllQuestions, OneQuestion
+from app.api.v1.views.question_views import Downvote, Upvote
+from app.api.v1.views.question_views import AllQuestions
 
 version1 = Blueprint('v1', __name__, url_prefix='/api/v1')
 
@@ -10,4 +11,7 @@ api = Api(version1, catch_all_404s=True)
 api.add_resource(AllMeetups, '/meetups', strict_slashes=False)
 api.add_resource(OneMeetup, '/meetups/<int:meetupId>', strict_slashes=False)
 api.add_resource(AllQuestions, '/questions', strict_slashes=False)
-api.add_resource(OneQuestion, '/questions/<int:questionId>', strict_slashes=False)
+api.add_resource(Upvote, '/questions/<int:questionId>/upvote',
+                 strict_slashes=False)
+api.add_resource(Downvote, '/questions/<int:questionId>/downvote',
+                 strict_slashes=False)
