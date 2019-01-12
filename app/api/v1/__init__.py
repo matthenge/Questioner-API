fc1e1df672ea17dea0a43c490dd043ad3fb60398
matthenge <matthenge@gmail.com>
2019-01-12 13:43:00 +0300

    [feature #163070591]Add a users endpoint

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -3,6 +3,7 @@ from flask_restful import Api, Resource
 from app.api.v1.views.meetup_views import AllMeetups, OneMeetup
 from app.api.v1.views.question_views import Downvote, Upvote
 from app.api.v1.views.question_views import AllQuestions
+from app.api.v1.views.user_views import Users
 
 version1 = Blueprint('v1', __name__, url_prefix='/api/v1')
 
@@ -15,3 +16,4 @@ api.add_resource(Upvote, '/questions/<int:questionId>/upvote',
                  strict_slashes=False)
 api.add_resource(Downvote, '/questions/<int:questionId>/downvote',
                  strict_slashes=False)
+api.add_resource(Users, '/auth/users', strict_slashes=False)
