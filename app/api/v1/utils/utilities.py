2d273d90027cb0d7ce142e5505dfcd1c64ff05d7
matthenge <matthenge@gmail.com>
2019-01-12 13:40:30 +0300

    [start #163070591]Modify Utilities page

--- app/api/v1/utils/utilities.py
+++ app/api/v1/utils/utilities.py
@@ -4,7 +4,7 @@
 class Helpers:
     """Helper methods"""
     def fetch(self, db, id):
-        """Fetch one question"""
+        """Fetch one item"""
         for key in db:
             if key == id:
                 return db[key]
