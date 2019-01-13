a9f50d02194c848d982981abc467dbf6c7e23407
matthenge <matthenge@gmail.com>
2019-01-13 18:36:21 +0300

    [feature #163070712]Added a login Endpoint

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -3,7 +3,7 @@ from flask_restful import Api, Resource
 from app.api.v1.views.meetup_views import AllMeetups, OneMeetup
 from app.api.v1.views.question_views import Downvote, Upvote
 from app.api.v1.views.question_views import AllQuestions
-from app.api.v1.views.user_views import Users
+from app.api.v1.views.user_views import Users, Login
 
 version1 = Blueprint('v1', __name__, url_prefix='/api/v1')
 
@@ -17,3 +17,4 @@ api.add_resource(Upvote, '/questions/<int:questionId>/upvote',
 api.add_resource(Downvote, '/questions/<int:questionId>/downvote',
                  strict_slashes=False)
 api.add_resource(Users, '/auth/users', strict_slashes=False)
+api.add_resource(Login, '/auth/users/login', strict_slashes=False)
