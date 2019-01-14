97f4401342cd0ad8e6b864dfa0abf5a16cec3e33
matthenge <matthenge@gmail.com>
2019-01-14 23:28:26 +0300

    [chore #163178267]Add utility methods for validations

--- app/api/v1/utils/utilities.py
+++ app/api/v1/utils/utilities.py
@@ -10,12 +10,6 @@ class Helpers:
             if key == id:
                 return db[key]
 
-    def check_username(self, users, username):
-        """check for email in the database"""
-        for user in users:
-            if username == user["username"]:
-                return user
-
     def hash_password(self, password, username):
         """method to hash user passwords"""
         salt = password + username
@@ -26,11 +20,3 @@ class Helpers:
         """Compare hashed passwords"""
         if password == hashed:
             return True
-        else:
-            return False
-
-    def check_email(self, users, email):
-        """check for email in the database"""
-        for user in users:
-            if email == user["email"]:
-                return user
