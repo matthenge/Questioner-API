d6d49f69de9e4f7b12a33df0d3b31750ec194faa
matthenge <matthenge@gmail.com>
2019-01-15 12:44:31 +0300

    [start #163217452]Refactor Users model to conform to OOP standards

--- app/api/v1/models/user_models.py
+++ app/api/v1/models/user_models.py
@@ -1,42 +1,46 @@
 """User models"""
 
-users = []
 
-
-class UserModels():
+class UserModels:
     """Class for user operations"""
+    users = []
 
-    def __init__(self):
+    def __init__(self, email, username, password, confirm_password):
         """Initialize the users models"""
-        self.db = users
+        self.userId = len(UserModels.users) + 1
+        self.email = email
+        self.username = username
+        self.password = password
+        self.confirm_password = confirm_password
+        self.isAdmin = True
 
-    def signup(self, email, username, password, confirm_password):
+    def signup(self):
         """method to signup"""
-        data = {
-            'userId': len(self.db) + 1,
-            'username': username,
-            'email': email,
-            'password': password,
-            'confirm_password': confirm_password,
-            'isAdmin': True
-        }
-        self.db.append(data)
-        return self.db
+        data = dict(
+            userId=self.userId,
+            email=self.email,
+            username=self.username,
+            password=self.password,
+            confirm_password=self.confirm_password,
+            isAdmin=self.isAdmin
+        )
+        self.users.append(data)
+        return self.users
 
     def fetch_username(self, username):
         """Method to fetch username"""
-        for user in self.db:
+        for user in UserModels.users:
             if user["username"] == username:
                 return user
 
     def fetch_email(self, email):
         """Method to fetch email"""
-        for user in self.db:
+        for user in UserModels.users:
             if user["email"] == email:
                 return user
 
     def fetch_userId(self, userId):
         """Method to fetch user by Id"""
-        for user in self.db:
+        for user in UserModels.users:
             if user["userId"] == userId:
                 return user
